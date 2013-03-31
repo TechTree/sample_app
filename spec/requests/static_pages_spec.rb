22f762071988f479343cd0d043c4c0678753a174
Vlad <vzasypkin@yahoo.com>
2013-03-31 22:12:34 +0000

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -52,7 +52,7 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign Up')
+    page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
     page.should have_selector 'title', text: 'Sample App'
   end
